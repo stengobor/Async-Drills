5210b597c232fd87fcda6cfe937b73825a6d6594
Sophie Engobor <sophietengobor@gmail.com>
2022-03-11 14:56:29 -0600

    just added something

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+function getPosition(options) {
+    if (!options) {
+        return Promise.reject(new Error('Options Needed'));
+    }
 
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    });
+}
+let value = 100;
+let promise = Promise.resolve(value);
+
+promise.then((returnValue) => {
+
+}).catch((err) => {
+    
+
+});
