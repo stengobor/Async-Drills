9cef9d6700a47cec7e81bdddaecf3dbc8ca29bea
Sophie Engobor <sophietengobor@gmail.com>
2022-03-13 15:38:40 -0500

    updated

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-function getPosition(options) {
-    if (!options) {
-        return Promise.reject(new Error('Options Needed'));
-    }
-
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject, options);
-    });
+// TIMING OUT
+// STEPS 1-3
+function greet() {
+  console.log("Hello!");
 }
-let value = 100;
-let promise = Promise.resolve(value);
 
-promise.then((returnValue) => {
+let timeOut;
 
-}).catch((err) => {
-    
+function myFunction() {
+  timeOut = setTimeout(greet, 2000);
+}
+greet();
+myFunction(greet);
 
-});
+// STEP 4
+const words = message => console.log(message);
+
+const getWords = () => {
+  words("Hello");
+
+  setTimeout(() => {
+    words("Again");
+
+    setTimeout(() => {
+      words("Whats");
+
+      setTimeout(() => {
+        words("Up");
+      }, 1000);
+    }, 2000);
+  }, 3000);
+};
+
+getWords();
+
+// CALLBACKS/RECURSION
